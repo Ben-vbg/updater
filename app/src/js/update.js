e8eec33dc1ac29e8a756c15ca2a2ec623bd18342
Ben-vbg <38947986+Ben-vbg@users.noreply.github.com>
2021-09-20 11:16:01 +0200

    up

--- app/src/js/update.js
+++ app/src/js/update.js
@@ -15,8 +15,8 @@ const updateOptions = {
   gitRepo: "updater", // [Required] Your Repo Name
   gitUsername: "Ben-vbg",  // [Required] Your GitHub Username.
 
-  appName: "Docky", //[Required] The Name of the app archive and the app folder.
-  appExecutableName: "Docky.app", //[Required] The Executable of the Application to be Run after updating.
+  appName: "docky", //[Required] The Name of the app archive and the app folder.
+  appExecutableName: "docky.app", //[Required] The Executable of the Application to be Run after updating.
 
   progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
   label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
