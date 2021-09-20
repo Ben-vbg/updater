9ef85849eda3e0d0ce065dcffc45d618c29684c2
Ben-vbg <38947986+Ben-vbg@users.noreply.github.com>
2021-09-20 11:04:30 +0200

    up

--- app/dist/js/update.js
+++ app/dist/js/update.js
@@ -15,11 +15,13 @@ const updateOptions = {
   gitRepo: "updater", // [Required] Your Repo Name
   gitUsername: "Ben-vbg",  // [Required] Your GitHub Username.
 
-  appName: "uaup-js", //[Required] The Name of the app archive and the app folder.
-  appExecutableName: "UAUP JS.exe", //[Required] The Executable of the Application to be Run after updating.
+  appName: "Docky", //[Required] The Name of the app archive and the app folder.
+  appExecutableName: "Docky.app", //[Required] The Executable of the Application to be Run after updating.
 
   progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
   label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
   forceUpdate: false, // {Default is false} [Optional] If the Application should be forced updated.  This will change to true if any errors ocurr while launching.
   stageTitles: defaultStages, // {Default is defaultStages} [Optional] Sets the Status Title for Each Stage
 };
+
+uaup.Update(updateOptions);
